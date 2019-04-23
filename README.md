bd1a1ac9a77c4ce3ef654019261c59a4d0bff7cd
Yasuharu Yanamura <yanamura3@gmail.com>
2019-04-23 10:52:55 +0900

    add install.sh

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-## emacs
-$ brew install emacs --with-cocoa
+```
+xcode-select --install
+sudo xcodebuild -license
+cd ~
+git clone https://github.com/yanamura/dotfiles.git ~/dotfiles
+./dotfiles/scripts/install.sh
+```
 
+## emacs
 M-x package-list-packages
 で
 helmとpopwinをいれる
---
+++ scripts/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#xcode-select --install
+#sudo xcodebuild -license
+
+# dotfiles
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.git-completion.bash ~/.git-completion.bash
+ln -s ~/dotfiles/.git_commit_template ~/.git_commit_template
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/.inputrc ~/.inputrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# brew
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+cd ~/dotfiles
+brew bundle
+
+
+# rbenv
+rbenv install 2.4
+rbenv global 2.4
+
+gem install bundler
